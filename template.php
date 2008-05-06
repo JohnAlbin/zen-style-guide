f9035a3e964e21786a76cb06ac8432945b8244ae
JohnAlbin <virtually.johnalbin@gmail.com>
2008-05-06 15:09:03 +0000

    Added $hook to preprocess documentation.

--- template.php
+++ template.php
@@ -70,9 +70,11 @@ function STARTERKIT_preprocess(&$vars, $hook) {
  *
  * @param $vars
  *   A sequential array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the theme function being called ("page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_page(&$vars) {
+function STARTERKIT_preprocess_page(&$vars, $hook) {
   // First run Zen's preprocess function.
   phptemplate_preprocess_page($vars);
 
@@ -85,9 +87,11 @@ function STARTERKIT_preprocess_page(&$vars) {
  *
  * @param $vars
  *   A sequential array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the theme function being called ("node" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_node(&$vars) {
+function STARTERKIT_preprocess_node(&$vars, $hook) {
   // First run Zen's preprocess function.
   phptemplate_preprocess_node($vars);
 
@@ -100,9 +104,11 @@ function STARTERKIT_preprocess_node(&$vars) {
  *
  * @param $vars
  *   A sequential array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the theme function being called ("comment" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_comment(&$vars) {
+function STARTERKIT_preprocess_comment(&$vars, $hook) {
   // First run Zen's preprocess function.
   phptemplate_preprocess_comment($vars);
 
@@ -115,9 +121,11 @@ function STARTERKIT_preprocess_comment(&$vars) {
  *
  * @param $vars
  *   A sequential array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the theme function being called ("block" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_block(&$vars) {
+function STARTERKIT_preprocess_block(&$vars, $hook) {
   // First run Zen's preprocess function.
   phptemplate_preprocess_block($vars);
 
