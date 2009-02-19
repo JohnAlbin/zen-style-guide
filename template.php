272b3998f51d984f3757a7a33ce94a3ce99abdc0
JohnAlbin <virtually.johnalbin@gmail.com>
2009-02-19 14:48:15 +0000

    More comments.

--- template.php
+++ template.php
@@ -130,6 +130,13 @@ function STARTERKIT_preprocess_page(&$vars, $hook) {
 /* -- Delete this line if you want to use this function
 function STARTERKIT_preprocess_node(&$vars, $hook) {
   $vars['sample_variable'] = t('Lorem ipsum.');
+
+  // Optionally, run node-type-specific preprocess functions, like
+  // STARTERKIT_preprocess_node_page() or STARTERKIT_preprocess_node_story().
+  $function = __FUNCTION__ . '_' . $vars['node']->type;
+  if (function_exists($function)) {
+    $function($vars);
+  }
 }
 // */
 
