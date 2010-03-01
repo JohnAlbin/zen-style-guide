ad337771c0f464663c227e4d866bebe2a23eaf25
JohnAlbin <virtually.johnalbin@gmail.com>
2010-03-01 17:37:28 +0000

    #729144 by Frank Ralf: Update STARTERKIT theme-settings.php for Drupal 7

--- theme-settings.php
+++ theme-settings.php
@@ -1,45 +1,29 @@
 <?php
 // $Id$
 
-// Include the definition of zen_settings() and zen_theme_get_default_settings().
-include_once './' . drupal_get_path('theme', 'zen') . '/theme-settings.php';
-
-
 /**
- * Implementation of THEMEHOOK_settings() function.
+ * Implements hook_form_system_theme_settings_alter().
  *
- * @param $saved_settings
- *   An array of saved settings for this theme.
- * @return
- *   A form array.
+ * @param $form
+ *   Nested array of form elements that comprise the form.
+ * @param $form_state
+ *   A keyed array containing the current state of the form.
  */
-function STARTERKIT_settings($saved_settings) {
+function STARTERKIT_form_system_theme_settings_alter(&$form, &$form_state)  {
 
-  // Get the default values from the .info file.
-  $defaults = zen_theme_get_default_settings('STARTERKIT');
+  // Create the form using Forms API: http://api.drupal.org/api/7
 
-  // Merge the saved variables and their default values.
-  $settings = array_merge($defaults, $saved_settings);
-
-  /*
-   * Create the form using Forms API: http://api.drupal.org/api/6
-   */
-  $form = array();
   /* -- Delete this line if you want to use this setting
   $form['STARTERKIT_example'] = array(
     '#type'          => 'checkbox',
-    '#title'         => t('Use this sample setting'),
-    '#default_value' => $settings['STARTERKIT_example'],
+    '#title'         => t('STARTERKIT sample setting'),
+    '#default_value' => theme_get_setting('STARTERKIT_example'),
     '#description'   => t("This option doesn't do anything; it's just an example."),
   );
   // */
 
-  // Add the base theme's settings.
-  $form += zen_settings($saved_settings, $defaults);
-
   // Remove some of the base theme's settings.
   unset($form['themedev']['zen_layout']); // We don't need to select the base stylesheet.
 
-  // Return the form
-  return $form;
+  // We are editing the $form in place, so we don't need to return anything.
 }
