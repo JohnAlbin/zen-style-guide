9c8a0170a245298c7381d194227f3cdd4d9907ee
JohnAlbin <virtually.johnalbin@gmail.com>
2011-02-19 02:02:05 +0000

    #1066436 by JohnAlbin: STARTERKIT_theme() no longer needs to call zen_theme()

--- template.php
+++ template.php
@@ -62,19 +62,6 @@
 
 
 /**
- * Implementation of HOOK_theme().
- */
-function STARTERKIT_theme(&$existing, $type, $theme, $path) {
-  $hooks = zen_theme($existing, $type, $theme, $path);
-  // Add your theme hooks like this:
-  /*
-  $hooks['hook_name_here'] = array( // Details go here );
-  */
-  // @TODO: Needs detailed comments. Patches welcome!
-  return $hooks;
-}
-
-/**
  * Override or insert variables into all templates.
  *
  * @param $vars
