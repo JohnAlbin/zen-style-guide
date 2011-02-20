e61523ef3907bf34c427cbe2e3c4b2e84bffab5a
JohnAlbin <virtually.johnalbin@gmail.com>
2011-02-20 15:03:19 +0000

    #1064706 by JohnAlbin: Use $variables instead of $vars to match D7 code snippets

--- template.php
+++ template.php
@@ -64,52 +64,52 @@
 /**
  * Override or insert variables into the html templates.
  *
- * @param $vars
+ * @param $variables
  *   An array of variables to pass to the theme template.
  * @param $hook
  *   The name of the template being rendered ("html" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_html(&$vars, $hook) {
-  $vars['sample_variable'] = t('Lorem ipsum.');
+function STARTERKIT_preprocess_html(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
 
   // The body tag's classes are controlled by the $classes_array variable. To
   // remove a class from $classes_array, use array_diff().
-  //$vars['classes_array'] = array_diff($vars['classes_array'], array('class-to-remove'));
+  //$variables['classes_array'] = array_diff($variables['classes_array'], array('class-to-remove'));
 }
 // */
 
 /**
  * Override or insert variables into the page templates.
  *
- * @param $vars
+ * @param $variables
  *   An array of variables to pass to the theme template.
  * @param $hook
  *   The name of the template being rendered ("page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_page(&$vars, $hook) {
-  $vars['sample_variable'] = t('Lorem ipsum.');
+function STARTERKIT_preprocess_page(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
 }
 // */
 
 /**
  * Override or insert variables into the node templates.
  *
- * @param $vars
+ * @param $variables
  *   An array of variables to pass to the theme template.
  * @param $hook
  *   The name of the template being rendered ("node" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_node(&$vars, $hook) {
-  $vars['sample_variable'] = t('Lorem ipsum.');
+function STARTERKIT_preprocess_node(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
 
   // Optionally, run node-type-specific preprocess functions, like
   // STARTERKIT_preprocess_node_page() or STARTERKIT_preprocess_node_story().
-  $function = __FUNCTION__ . '_' . $vars['node']->type;
+  $function = __FUNCTION__ . '_' . $variables['node']->type;
   if (function_exists($function)) {
-    $function($vars, $hook);
+    $function($variables, $hook);
   }
 }
 // */
@@ -117,28 +117,28 @@ function STARTERKIT_preprocess_node(&$vars, $hook) {
 /**
  * Override or insert variables into the comment templates.
  *
- * @param $vars
+ * @param $variables
  *   An array of variables to pass to the theme template.
  * @param $hook
  *   The name of the template being rendered ("comment" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_comment(&$vars, $hook) {
-  $vars['sample_variable'] = t('Lorem ipsum.');
+function STARTERKIT_preprocess_comment(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
 }
 // */
 
 /**
  * Override or insert variables into the block templates.
  *
- * @param $vars
+ * @param $variables
  *   An array of variables to pass to the theme template.
  * @param $hook
  *   The name of the template being rendered ("block" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_block(&$vars, $hook) {
+function STARTERKIT_preprocess_block(&$variables, $hook) {
   // Add a count to all the blocks in the region.
-  $vars['classes_array'][] = 'count-' . $vars['block_id'];
+  $variables['classes_array'][] = 'count-' . $variables['block_id'];
 }
 // */
