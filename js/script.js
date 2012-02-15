f660d8d588586e56a55bce4015d6e09762ec903e
maartenverbaarschot <maartenverbaarschot@1305466.no-reply.drupal.org>
2012-02-15 14:25:11 +0800

    Issue #1105868: Add needed JavaScript documentation to script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// adding a small wrapper around your JavaScript code. See:
+// - http://drupal.org/node/224333#javascript_compatibility
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($) {
+
+
+// Place your code here.
+
+
+})(jQuery);
