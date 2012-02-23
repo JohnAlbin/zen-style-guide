9cb1097483ff09e18e6a1f77825ec6f090e2d80b
JohnAlbin <virtually.johnalbin@gmail.com>
2012-02-24 01:00:37 +0800

    Issue #1105868: Update JavaScript documentation.

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@
  */
 
 // JavaScript should be made compatible with libraries other than jQuery by
-// adding a small wrapper around your JavaScript code. See:
-// - http://drupal.org/node/224333#javascript_compatibility
+// wrapping it with an "anonymous closure". See:
+// - http://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
-(function ($) {
+(function ($, Drupal, window, document, undefined) {
 
 
 // Place your code here.
 
 
-})(jQuery);
+})(jQuery, Drupal, this, this.document);
