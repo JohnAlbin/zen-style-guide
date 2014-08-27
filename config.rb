7a921cca90243b2184942576644fee94ed3ab578
JohnAlbin <virtually.johnalbin@gmail.com>
2014-08-27 20:04:11 +0800

    Simplify config.rb.

--- config.rb
+++ config.rb
@@ -7,41 +7,20 @@
 #
 
 
-# Change this to :production when ready to deploy the CSS to the live server.
-environment = :development
-#environment = :production
-
-# In development, we can turn on the FireSass-compatible debug_info.
-firesass = false
-#firesass = true
-
+# Require any additional compass plugins installed on your system.
+require 'breakpoint'
+require 'zen-grids'
 
 # Location of the theme's resources.
 css_dir         = "css"
 sass_dir        = "sass"
-extensions_dir  = "sass-extensions"
+fonts_dir       = "fonts"
 images_dir      = "images"
 javascripts_dir = "js"
 
-
-# Require any additional compass plugins installed on your system.
-#require 'breakpoint'
-require 'zen-grids'
-
-# Assuming this theme is in sites/*/themes/THEMENAME, you can add the partials
-# included with a module by uncommenting and modifying one of the lines below:
-#add_import_path "../../../default/modules/FOO"
-#add_import_path "../../../all/modules/FOO"
-#add_import_path "../../../../modules/FOO"
-
-
-##
-## You probably don't need to edit anything below this.
-##
-
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
@@ -49,8 +28,10 @@ output_style = (environment == :development) ? :expanded : :compressed
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
-# Pass options to sass. For development, we turn on the FireSass-compatible
-# debug_info if the firesass config variable above is true.
-sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
+# CSS sourcemaps
+sourcemap = false
+
+# Pass options to sass.
+# sass_options = {}
