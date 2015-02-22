31d572b98166ba32c728c97d8a99516c35a99d77
JohnAlbin <virtually.johnalbin@gmail.com>
2015-02-23 01:03:07 +0800

    Add Gulp compilation of KSS style guide.

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+'use strict';
+
+// Include Gulp & Tools We'll Use
+var gulp      = require('gulp'),
+  $           = require('gulp-load-plugins')(),
+  del         = require('del'),
+  runSequence = require('run-sequence'),
+
+// Task configuration.
+  theme         = __dirname + '/',
+  styleguide    = __dirname + '/styleguide/',
+
+// Get theme sub-directories from Compass' config.rb.
+  compass       = require('compass-options').dirs({'config': theme + 'config.rb'});
+
+
+// Build styleguide.
+gulp.task('styleguide', ['clean:styleguide', 'styleguide:markup'], $.shell.task([
+    // kss-node [source folder of files to parse] [destination folder] --template [location of template files]
+    'kss-node <%= source %> <%= destination %> --css ../css/styles.css'
+  ], {
+    templateData: {
+      source:       theme + compass.sass,
+      destination:  styleguide
+    }
+  }
+));
+
+gulp.task('styleguide:sass-colors', $.shell.task([
+    'bundle exec sass --compass --scss --sourcemap=none --style expanded sass/style-guide/sass-colors.scss.txt sass/style-guide/sass-colors.html.txt'
+  ], {cwd: theme}
+));
+
+gulp.task('styleguide:markup', ['styleguide:sass-colors'], $.shell.task([
+    'head -n 2  sass/style-guide/sass-colors.html.txt | tail -n 1 > sass/style-guide/sass-colors.html'
+  ], {cwd: theme}
+));
+
+
+// Lint JavaScript.
+gulp.task('lint:js', function () {
+  return gulp.src(theme + compass.js + '/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'));
+});
+
+// Lint Sass.
+gulp.task('lint:sass', function() {
+  return gulp.src(theme + compass.sass + '/**/*.scss')
+    .pipe($.scssLint({'bundleExec': true}));
+});
+
+// Lint Sass and JavaScript.
+gulp.task('lint', function (cb) {
+  runSequence(['lint:js', 'lint:sass'], cb);
+});
+
+// Build CSS.
+gulp.task('styles', ['clean:css'], $.shell.task([
+    'bundle exec compass compile --time --sourcemap --output-style expanded'
+  ], {cwd: theme}
+));
+
+gulp.task('styles:production', ['clean:css'], $.shell.task([
+    'bundle exec compass compile --time --no-sourcemap --output-style compressed'
+  ], {cwd: theme}
+));
+
+// Watch for front-end changes and rebuild on the fly.
+gulp.task('watch', ['clean:css', 'watch:files'],
+  // This task cannot be used in a dependency, since this task won't ever end
+  // due to "compass watch" never completing.
+  $.shell.task(
+    ['bundle exec compass watch --time --sourcemap --output-style expanded'],
+    {cwd: theme}
+  )
+);
+gulp.task('watch:files', ['styleguide', 'lint:sass'], function() {
+  return gulp.watch(theme + compass.sass + '/**/*.scss', ['styleguide', 'lint:sass']);
+});
+
+// Clean styleguide directory.
+gulp.task('clean:styleguide', del.bind(null, [styleguide + '*.html', styleguide + 'public'], {force: true}));
+
+// Clean CSS directory.
+gulp.task('clean:css', del.bind(null, [theme + '**/.sass-cache', theme + compass.css + '/**/*.css', theme + compass.css + '/**/*.map'], {force: true}));
+
+// Clean all directories.
+gulp.task('clean', ['clean:css', 'clean:styleguide']);
+
+// Production build of front-end.
+gulp.task('build', ['styles:production', 'styleguide'], function (cb) {
+  // Run linting last, otherwise its output gets lost.
+  runSequence(['lint'], cb);
+});
+
+// The default task.
+gulp.task('default', ['build']);
+
+
+// Resources used to create this gulpfile.js:
+// - https://github.com/google/web-starter-kit/blob/master/gulpfile.js
+// - https://github.com/north/generator-north/blob/master/app/templates/Gulpfile.js
